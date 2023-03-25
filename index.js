822b56ba15916bdbe573f8929e590e766475d61e
Pramitha Dewi <dipramithadewi@gmail.com>
2023-03-25 16:35:41 +0700

    Melakukan styling bagian profil dan form

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ function displayValue() {
     document.getElementById("labelRole").innerHTML = role;
 
     var availability = document.getElementById("inputAvailability").value;
-    document.getElementById("labelAvailability").innerHTML = 'Availability: ' + availability;
+    document.getElementById("labelAvailability").innerHTML = availability;
 
     var usia = document.getElementById("inputUsia").value;
-    document.getElementById("labelUsia").innerHTML = 'Usia: ' + usia;
+    document.getElementById("labelUsia").innerHTML = usia;
 
     var lokasi = document.getElementById("inputLokasi").value;
-    document.getElementById("labelLokasi").innerHTML = 'Lokasi: ' + lokasi;
+    document.getElementById("labelLokasi").innerHTML = lokasi;
 
     var pengalaman = document.getElementById("inputPengalaman").value;
-    document.getElementById("labelPengalaman").innerHTML = 'Pengalaman: ' + pengalaman;
+    document.getElementById("labelPengalaman").innerHTML = pengalaman;
 
     var email = document.getElementById("inputEmail").value;
-    document.getElementById("labelEmail").innerHTML = 'Email: ' + email;
+    document.getElementById("labelEmail").innerHTML = email;
 }
